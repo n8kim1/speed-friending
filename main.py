740deca025f88c5a58aa1ae012b9230de1c4e048
Nathaniel Kim <nkim8@mit.edu>
2021-04-11 19:49:44 -0400

    Add to section A member's group w/in rd

--- main.py
+++ main.py
@@ -17,51 +17,57 @@ pair_freqs["B1"] = {"B2": 0, "B3": 0, "A1": 0, "A2": 0, "A3": 0}
 pair_freqs["B2"] = {"B1": 0, "B3": 0, "A1": 0, "A2": 0, "A3": 0}
 pair_freqs["B3"] = {"B1": 0, "B2": 0, "A1": 1, "A2": 0, "A3": 0}
 
-# pair section 1 with section 2, if possible
-# TODO dynamically count this
-# TODO include downward scaling
-pairing_priority = dict()
-# (section1, section2)
-for a in section_1:
-    pairing_priority[a] = sum([pair_freqs[a][b] for b in pair_freqs[a] if b in section_2])
-
-pairing_order = sorted(pairing_priority.keys(), key=lambda key: pairing_priority[key], reverse=True)
-
 paired_this_round = []
 
-def viable_to_group(person):
-    for group in paired_this_round:
-        if person in group and len(group) >= group_size:
-            return False
-    return True
+is_new_pair_created = True
+while is_new_pair_created:
+    is_new_pair_created = False
+    # pair section 1 with section 2, if possible
+    # TODO dynamically count this
+    # TODO include downward scaling
+    pairing_priority = dict()
+    # (section1, section2)
+    for a in section_1:
+        pairing_priority[a] = sum([pair_freqs[a][b] for b in pair_freqs[a] if b in section_2])
 
-def in_same_group(a, b):
-    for group in paired_this_round:
-        if a in group and b in group:
-            return True
-    return False
+    pairing_order = sorted(pairing_priority.keys(), key=lambda key: pairing_priority[key], reverse=True)
 
-# TODO should pair_freqs be updated in the middle of a round?
-def add_to_group(a, b):
-    for group in paired_this_round:
-        if a in group:
-            group.add(b)
-            return
-        elif b in group:
-            group.add(a)
-            return
-    paired_this_round.append(set((a, b)))
+    def viable_to_group(person):
+        for group in paired_this_round:
+            if person in group and len(group) >= group_size:
+                return False
+        return True
 
-# pick a member of section 1, find a viable pair
-for a in pairing_order:
-    a_paired_with = None
-    for b in sorted(pair_freqs[a].keys(), key=lambda b: pair_freqs[a][b]):
-        if b in section_2 and viable_to_group(b) and not in_same_group(a, b):
-            a_paired_with = b
-            break
-    if a_paired_with is not None:
-        add_to_group(b, a)
-        print(a, b)
+    def in_same_group(a, b):
+        for group in paired_this_round:
+            if a in group and b in group:
+                return True
+        return False
+    def add_to_group(a, b):
+        pair_freqs[a][b] += 1
+        pair_freqs[b][a] += 1
+        for group in paired_this_round:
+            if a in group:
+                group.add(b)
+                return
+            elif b in group:
+                group.add(a)
+                return
+        paired_this_round.append(set((a, b)))
+
+    # pick a member of section 1, find a viable pair
+    for a in pairing_order:
+        if viable_to_group(a):
+            a_paired_with = None
+            for b in sorted(pair_freqs[a].keys(), key=lambda b: pair_freqs[a][b]):
+                if b in section_2 and viable_to_group(b) and not in_same_group(a, b):
+                    a_paired_with = b
+                    break
+            if a_paired_with is not None:
+                add_to_group(b, a)
+                print(a, b)
+                is_new_pair_created = True
+                break
 
 print(paired_this_round)
 
