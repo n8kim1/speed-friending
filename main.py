d55cbe165c3ceed7857b30295675cf8a39791c33
Nathaniel Kim <nkim8@mit.edu>
2021-04-12 21:38:59 -0400

    Move helper fns outside iteration

--- main.py
+++ main.py
@@ -21,6 +21,32 @@ pair_freqs["B3"] = {"B1": 0, "B2": 0, "A1": 1, "A2": 0, "A3": 0}
 
 paired_this_round = []
 
+# some helper functions
+def viable_to_group(person):
+    for group in paired_this_round:
+        if person in group and len(group) >= group_size:
+            return False
+    return True
+
+def in_same_group(a, b):
+    for group in paired_this_round:
+        if a in group and b in group:
+            return True
+    return False
+
+def add_to_group(a, b):
+    pair_freqs[a][b] += 1
+    pair_freqs[b][a] += 1
+    for group in paired_this_round:
+        if a in group:
+            group.add(b)
+            return
+        elif b in group:
+            group.add(a)
+            return
+    paired_this_round.append(set((a, b)))
+
+# main loop of code, iterating and creating pairs
 is_new_pair_created = True
 while is_new_pair_created:
     is_new_pair_created = False
@@ -33,29 +59,6 @@ while is_new_pair_created:
         pairing_priority[a] = sum([pair_freqs[a][b] for b in pair_freqs[a] if b in section_2])
 
     pairing_order = sorted(pairing_priority.keys(), key=lambda key: ((pairing_priority[key], random.random())), reverse=True)
-
-    def viable_to_group(person):
-        for group in paired_this_round:
-            if person in group and len(group) >= group_size:
-                return False
-        return True
-
-    def in_same_group(a, b):
-        for group in paired_this_round:
-            if a in group and b in group:
-                return True
-        return False
-    def add_to_group(a, b):
-        pair_freqs[a][b] += 1
-        pair_freqs[b][a] += 1
-        for group in paired_this_round:
-            if a in group:
-                group.add(b)
-                return
-            elif b in group:
-                group.add(a)
-                return
-        paired_this_round.append(set((a, b)))
 
     # pick a member of section 1, find a viable pair
     for a in pairing_order:
